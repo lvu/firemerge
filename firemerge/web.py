879189b92f25416923b7f5afed5a5c5d3d45abbb
Valentyn Lavrinenko <Valentin.Lavrinenko@gmail.com>
2024-10-21 21:49:09 +0300

    Preload reference info from server

--- firemerge/web.py
+++ firemerge/web.py
@@ -10,13 +10,16 @@ from dotenv import load_dotenv
 from pydantic import TypeAdapter
 
 from firemerge.firefly_client import FireflyClient
-from firemerge.model import StatementTransaction, Transaction
+from firemerge.model import Account, Category, Currency, StatementTransaction, Transaction
 
 
 PROJECT_ROOT = os.path.realpath(os.path.join(os.path.dirname(__file__), ".."))
 FRONTEND_ROOT = os.path.join(PROJECT_ROOT, "frontend")
 FIREFLY_CLIENT = web.AppKey("ff_client", FireflyClient)
 STATEMENT = web.AppKey("statement", list[StatementTransaction])
+ACCOUNTS = web.AppKey("accounts", list[Account])
+CATEGORIES = web.AppKey("categories", list[Category])
+CURRENCIES = web.AppKey("currencies", list[Currency])
 
 
 async def root(request: web.Request) -> web.Response:
@@ -30,21 +33,21 @@ async def statement(request: web.Request) -> web.Response:
 async def accounts(request: web.Request) -> web.Response:
     return web.json_response([
         row.model_dump(mode="json")
-        async for row in request.app[FIREFLY_CLIENT].get_accounts()
+        for row in request.app[ACCOUNTS]
     ])
 
 
 async def categories(request: web.Request) -> web.Response:
     return web.json_response([
         row.model_dump(mode="json")
-        async for row in request.app[FIREFLY_CLIENT].get_categories()
+        for row in request.app[CATEGORIES]
     ])
 
 
 async def currencies(request: web.Request) -> web.Response:
     return web.json_response([
         row.model_dump(mode="json")
-        async for row in request.app[FIREFLY_CLIENT].get_currencies()
+        for row in request.app[CURRENCIES]
     ])
 
 
@@ -64,6 +67,13 @@ def read_statement() -> Iterable[StatementTransaction]:
             yield StatementTransaction.model_validate_json(line)
 
 
+async def load_refs(app: web.Application) -> None:
+    ff_client = app[FIREFLY_CLIENT]
+    app[ACCOUNTS] = [x async for x in ff_client.get_accounts()]
+    app[CATEGORIES] = [x async for x in ff_client.get_categories()]
+    app[CURRENCIES] = [x async for x in ff_client.get_currencies()]
+
+
 def main():
     load_dotenv()
     app = web.Application()
@@ -78,6 +88,7 @@ def main():
         web.get('/categories', categories),
         web.get('/currencies', currencies),
     ])
+    app.on_startup.append(load_refs)
     web.run_app(app)
 
 
