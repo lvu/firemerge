289fc6e60ef9096e40568680f00a9be824b2d785
Valentyn Lavrinenko <Valentin.Lavrinenko@gmail.com>
2024-10-21 21:47:18 +0300

    get rid of config

--- firemerge/web.py
+++ firemerge/web.py
@@ -15,10 +15,8 @@ from firemerge.model import StatementTransaction, Transaction
 
 PROJECT_ROOT = os.path.realpath(os.path.join(os.path.dirname(__file__), ".."))
 FRONTEND_ROOT = os.path.join(PROJECT_ROOT, "frontend")
-CONFIG_PATH = os.path.join(PROJECT_ROOT, "config.toml")
 FIREFLY_CLIENT = web.AppKey("ff_client", FireflyClient)
 STATEMENT = web.AppKey("statement", list[StatementTransaction])
-CONFIG = web.AppKey("config", tomlkit.TOMLDocument)
 
 
 async def root(request: web.Request) -> web.Response:
@@ -60,32 +58,10 @@ async def transactions(request: web.Request) -> web.Response:
     ])
 
 
-async def config(request: web.Request) -> web.Response:
-    return web.json_response({
-        "assetAccount": request.app[CONFIG].get("asset_account"),
-    })
-
-
-async def save_config(request: web.Request) -> web.Response:
-    data = await request.json()
-    request.app[CONFIG]["asset_account"] = data["assetAccount"]
-    with open(CONFIG_PATH, "w") as f:
-        tomlkit.dump(request.app[CONFIG], f)
-    return web.Response()
-
-
 def read_statement() -> Iterable[StatementTransaction]:
     with open(sys.argv[1]) as f:
         for line in f:
             yield StatementTransaction.model_validate_json(line)
-
-
-def read_config() -> tomlkit.TOMLDocument:
-    try:
-        with open(CONFIG_PATH, "r") as f:
-            return tomlkit.load(f)
-    except FileNotFoundError:
-        return tomlkit.TOMLDocument()
 
 
 def main():
@@ -93,7 +69,6 @@ def main():
     app = web.Application()
     app[STATEMENT] = list(read_statement())
     app[FIREFLY_CLIENT] = FireflyClient(os.getenv("FIREFLY_BASE_URL"), os.getenv("FIREFLY_TOKEN"))
-    app[CONFIG] = read_config()
     app.router.add_static('/static/', path=FRONTEND_ROOT, name='static')
     app.add_routes([
         web.get('/', root),
@@ -102,8 +77,6 @@ def main():
         web.get('/accounts', accounts),
         web.get('/categories', categories),
         web.get('/currencies', currencies),
-        web.get('/config', config),
-        web.post('/config', save_config),
     ])
     web.run_app(app)
 
